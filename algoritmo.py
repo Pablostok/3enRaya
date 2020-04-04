88f398ee08de55a0040d0e919feeed8836c69868
Pablo V. R <pablitovr007@gmail.com>
2020-04-04 13:58:36 +0200

    Anti-perdida

---
+++ algoritmo.py
@@ -0,0 +1,106 @@
+import utilities
+
+numcol = 3
+numfil = 3
+
+matriz = utilities.damematriz(numcol, numfil)
+
+# -------------------------------------------------------------
+
+# j1 = input("Introduzca el nombre del jugador 1: ")
+# j2 = input("Introduzca el nombre del jugador 2: ")
+#
+# print(Jugador 1 + ": O")
+# print(Jugador 2 + ": X")
+
+# -------------------------------------------------------------
+listanegra = []
+
+# utilities.printmatriz(matriz, numcol, numfil)
+
+ok = False
+jugada = 0
+while ok == False:
+    print("")
+    print("   ---Turno del Jugador 1---   ")
+    print("")
+
+    posis = input("Introduzca la ~i~ de la posición a utilizar: ")
+    posjs = input("Introduzca la ~j~ de la posición a utilizar: ")
+
+    posi = int(posis)
+    posj = int(posjs)
+
+    listanegra.append([])
+    listanegra[jugada].append(posi)
+    listanegra[jugada].append(posj)
+    jugada = jugada + 1
+
+    for i in range(numfil):
+        for j in range(numcol):
+            if i == posi and j == posj:
+                matriz[i][j] = "O"
+
+    utilities.printmatriz(matriz, numcol, numfil)
+
+    ook = False
+    while ook == False:
+        print("")
+        print("   ---Turno del Jugador 2---   ")
+        print("")
+
+        ok = False
+
+        while ok == False:
+            posis = input("Introduzca la ~i~ de la posición a utilizar: ")
+            posjs = input("Introduzca la ~j~ de la posición a utilizar: ")
+
+            print(listanegra)
+            posi = int(posis)
+            posj = int(posjs)
+            print(listanegra)
+
+            ok = utilities.comprobaruso(listanegra, posi, posj)
+
+        listanegra.append([])
+        listanegra[jugada].append(posi)
+        listanegra[jugada].append(posj)
+        jugada = jugada + 1
+        print(listanegra)
+
+        for i in range(numfil):
+            for j in range(numcol):
+                if i == posi and j == posj:
+                    matriz[i][j] = "X"
+
+        utilities.printmatriz(matriz, numcol, numfil)
+
+        print("")
+        print("   ---Turno del Jugador 1---   ")
+        print("")
+
+        ok = False
+
+        while ok == False:
+            posis = input("Introduzca la ~i~ de la posición a utilizar: ")
+            posjs = input("Introduzca la ~j~ de la posición a utilizar: ")
+
+            print(listanegra)
+            posi = int(posis)
+            posj = int(posjs)
+            print(listanegra)
+
+            ok = utilities.comprobaruso(listanegra, posi, posj)
+
+        listanegra.append([])
+        listanegra[jugada].append(posi)
+        listanegra[jugada].append(posj)
+        jugada = jugada + 1
+        print(listanegra)
+
+        for i in range(numfil):
+            for j in range(numcol):
+                if i == posi and j == posj:
+                    matriz[i][j] = "O"
+
+        utilities.printmatriz(matriz, numcol, numfil)
--- utilities.py
+++ utilities.py
@@ -7,20 +7,84 @@ def damematriz(numcol, numfil):
     for i in range(numcol):
         matriz.append([])
         for j in range(numfil):
-            matriz[i].append(randrange(9))
+            matriz[i].append("#")
 
     for i in range(numcol):
         aux = ""
         for j in range(numfil):
             aux2 = str(matriz[i][j])
-            aux = aux + aux2 + " "
+            aux = "   " + aux + aux2 + "  "
         print(aux)
     return matriz
+
 
 def printmatriz(matriz, numcol, numfil):
     for i in range(numfil):
         aux = ""
         for j in range(numcol):
             aux2 = str(matriz[i][j])
-            aux = aux + aux2 + " "
+            aux = "    " + aux + aux2 + "  "
         print(aux)
+
+
+def comprobarganador(matriz):
+    # Combinaciones O
+
+    # horizontales
+    if matriz[0][0] and matriz[0][1] and matriz[0][2] == "O":
+        print("--- Gana ~O~ ---")
+    if matriz[1][0] and matriz[1][1] and matriz[1][2] == "O":
+        print("--- Gana ~O~ ---")
+    if matriz[2][0] and matriz[2][1] and matriz[2][2] == "O":
+        print("--- Gana ~O~ ---")
+
+    # verticales
+    if matriz[0][0] and matriz[1][0] and matriz[2][0] == "O":
+        print("--- Gana ~O~ ---")
+    if matriz[0][1] and matriz[1][1] and matriz[2][1] == "O":
+        print("--- Gana ~O~ ---")
+    if matriz[0][2] and matriz[1][2] and matriz[2][2] == "O":
+        print("--- Gana ~O~ ---")
+
+    # diagonales
+    if matriz[0][0] and matriz[1][1] and matriz[2][2] == "O":
+        print("--- Gana ~O~ ---")
+    if matriz[2][0] and matriz[1][1] and matriz[0][2] == "O":
+        print("--- Gana ~O~ ---")
+
+    # Combinaciones X
+
+    # horizontales
+    if matriz[0][0] and matriz[0][1] and matriz[0][2] == "X":
+        print("--- Gana ~X~ ---")
+    if matriz[1][0] and matriz[1][1] and matriz[1][2] == "X":
+        print("--- Gana ~X~ ---")
+    if matriz[2][0] and matriz[2][1] and matriz[2][2] == "X":
+        print("--- Gana ~X~ ---")
+
+    # verticales
+    if matriz[0][0] and matriz[1][0] and matriz[2][0] == "X":
+        print("--- Gana ~X~ ---")
+    if matriz[0][1] and matriz[1][1] and matriz[2][1] == "X":
+        print("--- Gana ~X~ ---")
+    if matriz[0][2] and matriz[1][2] and matriz[2][2] == "X":
+        print("--- Gana ~X~ ---")
+
+    # diagonales
+    if matriz[0][0] and matriz[1][1] and matriz[2][2] == "X":
+        print("--- Gana ~X~ ---")
+    if matriz[2][0] and matriz[1][1] and matriz[0][2] == "X":
+        print("--- Gana ~X~ ---")
+
+
+def comprobaruso(listanegra, posi, posj):
+    leido = len(listanegra)
+    ok = False
+    o = 0
+
+    for i in range(0, leido):
+        if posi == listanegra[i][o] and posj == listanegra[i][o + 1]:
+            print("--- Error --- Casilla ocupada ---")
+            return False
+        else:
+            return True

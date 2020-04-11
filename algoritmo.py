e7dd884f382891e4eb4d1d3afb6d682e35cbd868
Pablo V. R <pablitovr007@gmail.com>
2020-04-11 12:15:57 +0200

    Mejora y limpieza

--- algoritmo.py
+++ algoritmo.py
@@ -1,26 +1,25 @@
 import utilities
 
 def juego():
+    numcol = 3
+    numfil = 3
+
     jugadores = utilities.recogidadatos()
     j1 = jugadores[0]
     j2 = jugadores[1]
 
     print(j1 + ": O")
-    print(j1 + ": X")
+    print(j2 + ": X")
 
-    numcol = 3
-    numfil = 3
+    # ----------------------------------------------------
+    matriz = utilities.damematriz(numcol, numfil)  # Hacer matriz e imprimirla
+    # -----------------------------------------------------
 
-    matriz = utilities.damematriz(numcol, numfil)
-
-    # listanegra = []
-    # listaAux = []
     jugador = ""
-
-    ok = False
     jugada = 1
 
-    while ok == False:
+    salir = False
+    while salir == False:
 
         if jugada % 2 != 0:
             jugador = j1
@@ -54,77 +53,3 @@ def juego():
         jugada = jugada + 1
 
         salir = utilities.comprobarganador(matriz, ficha, jugador)
-
-        if salir == True:
-            ok = True
-
-        # ook = False
-        # while ook == False:
-        #     print("")
-        #     print("   ---Turno del Jugador 2 (" + j2 + ") ---   ")
-        #     print("")
-        #
-        #     ok = False
-        #
-        #     while ok == False:
-        #         posis = input("Introduzca la ~i~ de la posición a utilizar: ")
-        #         posjs = input("Introduzca la ~j~ de la posición a utilizar: ")
-        #         print("")
-        #
-        #         posi = int(posis)
-        #         posj = int(posjs)
-        #
-        #         listaAux = [posi, posj]
-        #         ok = utilities.comprobaruso(listanegra, listaAux)
-        #
-        #     listaAux = []
-        #     listanegra.append([])
-        #     listanegra[jugada].append(posi)
-        #     listanegra[jugada].append(posj)
-        #     jugada = jugada + 1
-        #
-        #     for i in range(numfil):
-        #         for j in range(numcol):
-        #             if i == posi and j == posj:
-        #                 matriz[i][j] = "X"
-        #
-        #     utilities.printmatriz(matriz, numcol, numfil)
-        #
-        #     salir = utilities.comprobarganador(matriz)
-        #
-        #     if salir == True:
-        #         ook = True
-        #
-        #     print("")
-        #     print("   ---Turno del Jugador 1 (" + j1 + ") ---   ")
-        #     print("")
-        #
-        #     ok = False
-        #
-        #     while ok == False:
-        #         posis = input("Introduzca la ~i~ de la posición a utilizar: ")
-        #         posjs = input("Introduzca la ~j~ de la posición a utilizar: ")
-        #
-        #         posi = int(posis)
-        #         posj = int(posjs)
-        #
-        #         listaAux = [posi, posj]
-        #         ok = utilities.comprobaruso(listanegra, listaAux)
-        #
-        #     listaAux = []
-        #     listanegra.append([])
-        #     listanegra[jugada].append(posi)
-        #     listanegra[jugada].append(posj)
-        #     jugada = jugada + 1
-        #
-        #     for i in range(numfil):
-        #         for j in range(numcol):
-        #             if i == posi and j == posj:
-        #                 matriz[i][j] = "O"
-        #
-        #     utilities.printmatriz(matriz, numcol, numfil)
-        #
-        #     salir = utilities.comprobarganador(matriz)
-        #
-        #     if salir == True:
-        #         ook = True
